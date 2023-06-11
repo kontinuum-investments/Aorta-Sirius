345e7b247842ad32bbac9b09684860b0d72fba8f
Kavindu Athaudha <kavindu@k-ih.co.uk>
2023-06-11 16:15:50 +1200

    Added a dev package.

--- upload_to_pypi.bat
+++ upload_to_pypi.bat
@@ -1,5 +1,5 @@
 del /S /Q dist\*
 del /S /Q src\aorta_sirius.egg-info
 venv\Scripts\pip install -r requirements-dev.txt
-venv\Scripts\python setup.py sdist %datetimef%
+venv\Scripts\python setup.py sdist
 venv\Scripts\twine upload -u __token__ -p %PYPI_API_TOKEN% dist\*
