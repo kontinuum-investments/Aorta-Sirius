b95f0b4ea64b29c97a34c85318042eb4be694581
Kavindu Athaudha <kavindu@k-ih.co.uk>
2023-06-26 12:25:34 +1200

    AORTA-10 Setup Discord messaging API

--- src/sirius/communication/discord/__init__.py
+++ src/sirius/communication/discord/__init__.py
@@ -30,9 +30,28 @@ class DiscordHTTPModel(HTTPModel):
 class Bot(DiscordHTTPModel):
     id: int
     username: str
+    server_list: List["Server"] = []
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
+
+    async def get_server(self, server_name: str) -> "Server":
+        if len(self.server_list) == 0:
+            self.server_list = await Server.get_all_servers()
+
+        server_list: List[Server] = list(filter(lambda s: s.name == server_name, self.server_list))
+
+        if len(server_list) == 1:
+            return server_list[0]
+        elif len(server_list) == 0:
+            raise ServerNotFoundException(f"Server not found\n"
+                                          f"Bot Name: {self.username}\n"
+                                          f"Server Name: {server_name}\n")
+        else:
+            raise DuplicateServersFoundException(f"Duplicate servers found\n"
+                                                 f"Bot Name: {self.username}\n"
+                                                 f"Server Name: {server_name}\n"
+                                                 f"Number of servers: {len(server_list)}\n")
 
     @classmethod
     @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Get Bot")
@@ -43,33 +62,30 @@ class Bot(DiscordHTTPModel):
 class Server(DiscordHTTPModel):
     id: int
     name: str
+    text_channel_list: List["TextChannel"] = []
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
-    @classmethod
-    @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Get Server")
-    async def get(cls, bot: Optional[Bot] = None, server: Optional[ServerName] = None) -> "Server":
-        if bot is None:
-            bot = await Bot.get()
+    async def get_text_channel(self, text_channel_name: str) -> "TextChannel":
+        if len(self.text_channel_list) == 0:
+            self.text_channel_list = await TextChannel.get_all(self)
 
-        if server is None:
-            server = ServerName(common.get_environmental_variable(EnvironmentVariable.DISCORD_SERVER_NAME))
+        text_channel_list: List[TextChannel] = list(filter(lambda t: t.name == text_channel_name, self.text_channel_list))
 
-        server_name: str = server.value if common.is_production_environment() else server.value + " [Dev]"
-        server_list: List[Server] = list(filter(lambda s: s.name == server_name, await Server.get_all_servers()))
-
-        if len(server_list) == 1:
-            return server_list[0]
-        elif len(server_list) == 0:
-            raise ServerNotFoundException(f"Server not found\n"
-                                          f"Bot Name: {bot.username}\n"
-                                          f"Server Name: {server_name}\n")
+        if len(text_channel_list) == 1:
+            return text_channel_list[0]
+        elif len(text_channel_list) == 0:
+            logger.warning("Channel not found; creating channel\n"
+                           f"Server Name: {self.name}\n"
+                           f"Channel Name: {text_channel_name}\n"
+                           )
+            return await TextChannel.create(text_channel_name, self)
         else:
-            raise DuplicateServersFoundException(f"Duplicate servers found\n"
-                                                 f"Bot Name: {bot.username}\n"
-                                                 f"Server Name: {server_name}\n"
-                                                 f"Number of servers: {len(server_list)}\n")
+            raise DuplicateServersFoundException(f"Duplicate channels found\n"
+                                                 f"Server Name: {self.name}\n"
+                                                 f"Channel Name: {text_channel_name}\n"
+                                                 f"Number of channels: {len(text_channel_list)}\n")
 
     @classmethod
     @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Get all Servers")
@@ -93,13 +109,7 @@ class Channel(DiscordHTTPModel):
 
     @classmethod
     @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Create Channel")
-    async def create(cls, channel_name: str, type_id: int, bot: Optional[Bot] = None, server: Optional[Server] = None) -> "Channel":
-        if bot is None:
-            bot = await Bot.get()
-
-        if server is None:
-            server = await Server.get(bot)
-
+    async def create(cls, channel_name: str, type_id: int, server: Server) -> "Channel":
         url: str = constants.ENDPOINT__CHANNEL__CREATE_CHANNEL_OR_GET_ALL_CHANNELS.replace("<Server_ID>", str(server.id))
         data: Dict[str, Any] = {"name": channel_name, "type": type_id}
         return await HTTPModel.post_return_one(url, data=data)  # type: ignore[return-value]
@@ -115,43 +125,13 @@ class TextChannel(Channel):
         await HTTPRequest.post(url, data={"content": message}, headers=self._headers)
 
     @classmethod
-    @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Get Text Channel")
-    async def get(cls, text_channel_name: str, bot: Optional[Bot] = None, server: Optional[Server] = None) -> "TextChannel":
-        if bot is None:
-            bot = await Bot.get()
-
-        if server is None:
-            server = await Server.get(bot)
-
-        text_channel_list: List[TextChannel] = list(filter(lambda t: t.name == text_channel_name, await TextChannel.get_all(server)))
-
-        if len(text_channel_list) == 1:
-            return text_channel_list[0]
-        elif len(text_channel_list) == 0:
-            logger.warning("Channel not found; creating channel\n"
-                           f"Bot Name: {bot.username}\n"
-                           f"Server Name: {server.name}\n"
-                           f"Channel Name: {text_channel_name}\n"
-                           )
-            return await TextChannel.create(text_channel_name)
-        else:
-            raise DuplicateServersFoundException(f"Duplicate channels found\n"
-                                                 f"Bot Name: {bot.username}\n"
-                                                 f"Server Name: {server.name}\n"
-                                                 f"Channel Name: {text_channel_name}\n"
-                                                 f"Number of channels: {len(text_channel_list)}\n")
-
-    @classmethod
     @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Get all Text Channels")
-    async def get_all(cls, server: Optional[Server] = None) -> List["TextChannel"]:
-        if server is None:
-            server = await Server.get()
-
+    async def get_all(cls, server: Server) -> List["TextChannel"]:
         channel_list: List[Channel] = list(filter(lambda c: c.type == 0, await Channel.get_all_channels(server)))
         return [TextChannel(**channel.dict()) for channel in channel_list]
 
     @classmethod
     @application_performance_monitoring.transaction(Operation.AORTA_SIRIUS, "Create a Text Channel")
-    async def create(cls, text_channel_name: str, type_id: int = 0, bot: Optional[Bot] = None, server: Optional[Server] = None) -> "TextChannel":
+    async def create(cls, text_channel_name: str, server: Server, type_id: int = 0) -> "TextChannel":
         channel: Channel = await Channel.create(text_channel_name, type_id)
         return TextChannel(**channel.dict())
