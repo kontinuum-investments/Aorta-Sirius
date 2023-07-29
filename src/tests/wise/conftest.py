c9268fee47bf0c1b95bd85996e39d2bab73ea178
Kavindu Athaudha <kavindu@kih.com.sg>
2023-07-29 13:27:42 +1200

    AORTA-13 Integrate the Wise API
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- src/tests/wise/conftest.py
+++ src/tests/wise/conftest.py
@@ -2,9 +2,6 @@ import asyncio
 from typing import Generator
 
 import pytest
-from _decimal import Decimal
-
-from sirius.wise import WiseAccount, WiseAccountType
 
 
 @pytest.fixture(scope="session")
@@ -12,15 +9,14 @@ def event_loop() -> Generator:
     policy = asyncio.get_event_loop_policy()
     loop = policy.new_event_loop()
     yield loop
-    loop.run_until_complete(clean_up_sandbox_environment())
+    # loop.run_until_complete(clean_up_sandbox_environment())
     loop.close()
 
-
-async def clean_up_sandbox_environment() -> None:
-    wise_account: WiseAccount = await WiseAccount.get(WiseAccountType.PRIMARY)
-    await wise_account.personal_profile._complete_all_transfers()
-    await wise_account.business_profile._complete_all_transfers()
-
-    for reserve_account in wise_account.personal_profile.reserve_account_list:
-        await reserve_account._set_balance(Decimal("0"))
-        await reserve_account.close()
+# async def clean_up_sandbox_environment() -> None:
+#     wise_account: WiseAccount = await WiseAccount.get(WiseAccountType.PRIMARY)
+#     await wise_account.personal_profile._complete_all_transfers()
+#     await wise_account.business_profile._complete_all_transfers()
+#
+#     for reserve_account in wise_account.personal_profile.reserve_account_list:
+#         await reserve_account._set_balance(Decimal("0"))
+#         await reserve_account.close()
