72f9f79353b6a6947b7396076fb261a6e39d7cec
Kavindu Athaudha <kavindu@kih.com.sg>
2023-07-29 13:11:03 +1200

    AORTA-13 Integrate the Wise API
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- src/tests/wise/conftest.py
+++ src/tests/wise/conftest.py
@@ -1,19 +1,25 @@
 import asyncio
-from asyncio import AbstractEventLoop
 
 import pytest
-import pytest_asyncio
+from _decimal import Decimal
 
 from sirius.wise import WiseAccount, WiseAccountType
 
 
 @pytest.fixture(scope="session")
-def event_loop() -> AbstractEventLoop:
-    return asyncio.get_event_loop()
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.run_until_complete(clean_up_sandbox_environment())
+    loop.close()
 
 
-@pytest_asyncio.fixture(autouse=True, scope="session")
-async def complete_all_transfer() -> None:
+async def clean_up_sandbox_environment() -> None:
     wise_account: WiseAccount = await WiseAccount.get(WiseAccountType.PRIMARY)
     await wise_account.personal_profile._complete_all_transfers()
     await wise_account.business_profile._complete_all_transfers()
+
+    for reserve_account in wise_account.personal_profile.reserve_account_list:
+        await reserve_account._set_balance(Decimal("0"))
+        await reserve_account.close()
