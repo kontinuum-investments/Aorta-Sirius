48397d95b9c5dc0176bd2aee7e66cdcf1b3d7542
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 17:05:21 +1200

    VITA-22 Setup Wise Hooks

--- src/sirius/wise/__init__.py
+++ src/sirius/wise/__init__.py
@@ -299,27 +299,27 @@ class CashAccount(Account):
         if isinstance(to_account, CashAccount):
             transfer = Transfer.intra_cash_account_transfer(self.profile, self, to_account, amount, is_amount_in_from_currency)
             await WiseDiscord.notify(f"**Intra-Account Transfer**:\n"
-                                 f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
-                                 f"From: *{self.currency.value}*\n"
-                                 f"To: *{to_account.currency.value}*\n"
-                                 f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n"
+                                     f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
+                                     f"From: *{self.currency.value}*\n"
+                                     f"To: *{to_account.currency.value}*\n"
+                                     f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n"
                                      )
 
         elif isinstance(to_account, ReserveAccount):
             transfer = Transfer.cash_to_savings_account_transfer(self.profile, self, to_account, amount)
             await WiseDiscord.notify(f"**Intra-Account Transfer**:\n"
-                                 f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
-                                 f"From: *{self.currency.value}*\n"
-                                 f"To: *{to_account.name}*\n"
-                                 f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n")
+                                     f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
+                                     f"From: *{self.currency.value}*\n"
+                                     f"To: *{to_account.name}*\n"
+                                     f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n")
 
         elif isinstance(to_account, Recipient):
             transfer = Transfer.cash_to_third_party_cash_account_transfer(self.profile, self, to_account, amount, "" if reference is None else reference, is_amount_in_from_currency)
             await WiseDiscord.notify(f"**Third-Party Transfer**:\n"
-                                 f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
-                                 f"From: *{self.currency.value}*\n"
-                                 f"To: *{to_account.account_holder_name}*\n"
-                                 f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n")
+                                     f"Timestamp: {get_timestamp_string(datetime.datetime.now())}\n"
+                                     f"From: *{self.currency.value}*\n"
+                                     f"To: *{to_account.account_holder_name}*\n"
+                                     f"Amount: *{self.currency.value} {'{:,}'.format(amount)}*\n")
 
             if not common.is_production_environment():
                 self._simulate_completed_transfer(transfer.id)
@@ -399,10 +399,10 @@ class ReserveAccount(Account):
 
         transfer: Transfer = Transfer.savings_to_cash_account_transfer(self.profile, self, to_account, amount)
         await WiseDiscord.notify(f"**Intra-Account Transfer**:\n"
-                             f"*Timestamp*: {get_timestamp_string(datetime.datetime.now())}\n"
-                             f"*From*: {self.name}\n"
-                             f"*To*: {to_account.currency.value}\n"
-                             f"*Amount*: {self.currency.value} {'{:,}'.format(amount)}\n")
+                                 f"*Timestamp*: {get_timestamp_string(datetime.datetime.now())}\n"
+                                 f"*From*: {self.name}\n"
+                                 f"*To*: {to_account.currency.value}\n"
+                                 f"*Amount*: {self.currency.value} {'{:,}'.format(amount)}\n")
 
         self.profile.wise_account._initialize()
         return transfer
@@ -668,6 +668,35 @@ class DebitCard(DataClass):
         ) for data in response.data["cards"]]
 
 
+class AccountCredit(DataClass):
+    account: CashAccount
+    is_attempted: bool
+    is_successful: bool
+    timestamp: datetime.datetime
+
+    @staticmethod
+    def get_from_request_data(request_data: Dict[str, Any]) -> "AccountCredit":
+        personal_profile: PersonalProfile = WiseAccount.get(WiseAccountType.PRIMARY).personal_profile
+        cash_account: CashAccount = next(filter(lambda c: c.id == request_data["data"]["resource"]["account_id"], personal_profile.cash_account_list))
+
+        return AccountCredit(account=cash_account,
+                             is_attempted=request_data["data"]["current_state"] == "incoming_payment_waiting",
+                             is_successful=request_data["data"]["current_state"] == "outgoing_payment_sent",
+                             timestamp=request_data["data"]["occurred_at"])
+
+
+class AccountDebit(DataClass):
+    id: int
+    account: CashAccount
+    timestamp: datetime.datetime
+
+    @staticmethod
+    def get_from_request_data(request_data: Dict[str, Any]) -> "AccountDebit":
+        personal_profile: PersonalProfile = WiseAccount.get(WiseAccountType.PRIMARY).personal_profile
+        cash_account: CashAccount = personal_profile.get_cash_account(Currency(request_data["data"]["currency"]))
+        return AccountDebit(id=request_data["data"]["resource"]["id"], account=cash_account, timestamp=request_data["data"]["occurred_at"])
+
+
 WiseAccount.model_rebuild()
 Profile.model_rebuild()
 PersonalProfile.model_rebuild()
@@ -675,3 +704,5 @@ BusinessProfile.model_rebuild()
 Account.model_rebuild()
 DebitCard.model_rebuild()
 Transaction.model_rebuild()
+AccountCredit.model_rebuild()
+AccountDebit.model_rebuild()
