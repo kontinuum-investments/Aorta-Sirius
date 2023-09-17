c9e5f6afafcd0d224c63888b34ffa81b516ccb4b
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-18 07:52:18 +1200

    AORTA-34 Integrate Excel

--- src/sirius/excel/__init__.py
+++ src/sirius/excel/__init__.py
@@ -3,6 +3,16 @@ from typing import Any, Dict, List
 import openpyxl
 from _decimal import Decimal
 
+from sirius.exceptions import SDKClientException
+
+
+class NotAKeyValuePairException(SDKClientException):
+    pass
+
+
+def _get_cell_value_from_cell(cell_value: Any) -> Any:
+    return Decimal(str(cell_value)) if isinstance(cell_value, (int, float)) and not isinstance(cell_value, bool) else cell_value
+
 
 def get_excel_data(file_path: str, sheet_name: str) -> List[Dict[Any, Any]]:
     workbook = openpyxl.load_workbook(filename=file_path, data_only=True)
@@ -18,7 +28,7 @@ def get_excel_data(file_path: str, sheet_name: str) -> List[Dict[Any, Any]]:
             excel_data: Dict[Any, Any] = {}
             cell_number: int = 0
             for cell in row:
-                excel_data[headers[cell_number]] = Decimal(str(cell.value)) if isinstance(cell.value, (int, float)) and not isinstance(cell.value, bool) else cell.value
+                excel_data[headers[cell_number]] = _get_cell_value_from_cell(cell.value)
                 cell_number = cell_number + 1
 
             excel_data_list.append(excel_data)
@@ -26,3 +36,16 @@ def get_excel_data(file_path: str, sheet_name: str) -> List[Dict[Any, Any]]:
         row_number = row_number + 1
 
     return excel_data_list
+
+
+def get_key_value_pair(file_path: str, sheet_name: str) -> Dict[Any, Any]:
+    workbook = openpyxl.load_workbook(filename=file_path, data_only=True)
+    key_value_pair: Dict[Any, Any] = {}
+
+    for row in workbook[sheet_name]:
+        if len(row) != 2:
+            raise NotAKeyValuePairException()
+
+        key_value_pair[str(row[0].value)] = _get_cell_value_from_cell(row[1].value)
+
+    return key_value_pair
